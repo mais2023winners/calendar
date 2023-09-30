fdcbba32ea527093fd963ca3909d05f67495a29c
LeiLiYang <liyanglei23@gmail.com>
2023-09-30 17:31:12 -0400

    added frontend files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Theme from "./Theme";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import Chat from "./pages/Chat";
 
 export default function App() {
   return <Theme>
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Chat/> 
+    </ChakraProvider>
   </Theme>
 }
