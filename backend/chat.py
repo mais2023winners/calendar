a1547136412db8399f6bdb07137eea4ca4e64a5a
Ahmed Mossa <ahmedmossa@Ahmeds-MacBook-Air.local>
2023-09-30 16:13:46 -0400

    Created a flask app & tweaked chat.py to make it a function

--- backend/chat.py
+++ backend/chat.py
@@ -3,77 +3,78 @@ import json
 
 from main import createEvent
 
-openai.api_key = "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"
+def ConnectToGPT(userMessage: str):
+    openai.api_key = "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"
 
-prompt = """
+    prompt = """
 
-You are a calendar chat bot, and you do 3 things:
-- Help the user find availabilites for a meeting. You will be given a list of events, and you will need to return a list of available time slots for a meeting. The events are sorted by start time in ascending order. You may assume that the list of events is non-empty. Furthermore, the start time of an event will always be before the end time.
-- Help the user schedule a meeting.
-- Help the user send an email if it is asked.
+    You are a calendar chat bot, and you do 3 things:
+    - Help the user find availabilites for a meeting. You will be given a list of events, and you will need to return a list of available time slots for a meeting. The events are sorted by start time in ascending order. You may assume that the list of events is non-empty. Furthermore, the start time of an event will always be before the end time.
+    - Help the user schedule a meeting.
+    - Help the user send an email if it is asked.
 
-Always respond in this JSON format:
+    Always respond in this JSON format:
 
-{
-    chatMessage: {
-        message: string,
-    }
-    payload?: {
-        type: "calendar_invite" | "email",
-        data: 
-        // if type is "calendar_invite"
-        {
-            startDate: Date,
-            endDate: Date,
-            // name of the event
-            summary: string,
-            description?: string,
-            attendeesEmailAddresses?: string[]  
-            location?: string
+    {
+        chatMessage: {
+            message: string,
         }
-        // if type is "email"
-        {
-            to: string[],
-            subject: string,
-            body: string
+        payload?: {
+            type: "calendar_invite" | "email",
+            data: 
+            // if type is "calendar_invite"
+            {
+                startDate: Date,
+                endDate: Date,
+                // name of the event
+                summary: string,
+                description?: string,
+                attendeesEmailAddresses?: string[]  
+                location?: string
+            }
+            // if type is "email"
+            {
+                to: string[],
+                subject: string,
+                body: string
+            }
         }
     }
-}
 
-Attribute responseMessage is always required.
-If user asks you to perform an action, always populate the payload attribute properly.
+    Attribute responseMessage is always required.
+    If user asks you to perform an action, always populate the payload attribute properly.
 
-Remember, always respond using the required JSON format.
+    Remember, always respond using the required JSON format.
 
-```
+    ```
 
-"""
+    """
 
-completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    temperature=0,
-    messages=[
-        {"role": "system", "content": prompt},
-        {
-            "role": "user",
-            "content": "Hello! create an event for my calendar on october 17th 2023 at 5pm for 1 hour period. In that period, i want to do homework for my ECSE 223 class. Name the event `it finally worked`",
-        },
-    ],
-)
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        temperature=0,
+        messages=[
+            {"role": "system", "content": prompt},
+            {
+                "role": "user",
+                "content": userMessage,
+            },
+        ],
+    )
 
-print(completion.choices[0].message)
-responseMessage = json.loads(completion.choices[0].message.content)["responseMessage"]
-print(responseMessage)
-payload = json.loads(completion.choices[0].message.content).get("payload")
+    print(completion.choices[0].message)
+    responseMessage = json.loads(completion.choices[0].message.content)["responseMessage"]
+    print(responseMessage)
+    payload = json.loads(completion.choices[0].message.content).get("payload")
 
-if payload.type == "calendar_invite":
-    print(createEvent(payload.data))
+    if payload.type == "calendar_invite":
+        print(createEvent(payload.data))
 
-elif payload.get("type") == "email":
-    data = payload.get("data")
-elif payload.get("type") == "calendar_invite":
-    date = payload.get("calendar_invite")
+    elif payload.get("type") == "email":
+        data = payload.get("data")
+    elif payload.get("type") == "calendar_invite":
+        date = payload.get("calendar_invite")
 
 
-print(responseMessage)
-print(payload)
+    print(responseMessage)
+    print(payload)
